03f76a00c4c28242825aad73d08ab85a44bc6bbd
YuxingLiu <yuxingliu0822@gmail.com>
2017-12-10 17:49:10 -0500

    update process noise parameters and initial state covariance

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 6;
+  std_a_ = 1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 1;
+  std_yawdd_ = 0.5;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -115,9 +115,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     // initialize state covariance matrix
     P_ << 1, 0, 0, 0, 0,
           0, 1, 0, 0, 0,
-          0, 0, 100, 0, 0,
-          0, 0, 0, 100, 0,
-          0, 0, 0, 0, 100;
+          0, 0, 1, 0, 0,
+          0, 0, 0, 1, 0,
+          0, 0, 0, 0, 1;
 
     time_us_ = meas_package.timestamp_;
     is_initialized_ = true;
